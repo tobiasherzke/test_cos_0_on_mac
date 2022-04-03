104be4a8415631bc9037eaba57a8e9320a58feb3
tobias <tobiasherzke@openmha.com>
2022-04-03 12:13:17 +0200

    Minimize

--- main.cpp
+++ main.cpp
@@ -2,13 +2,12 @@
 #include <cstdlib>
 
 int main(int, char**) {
-  auto r = random();
-  // buf will always contain 1.0 but compiler does not know.
-  float buf = (r != 1234) ? 1.0f : 0.567891234f;
+  // buf will always contain 1 but compiler does not know.
+  float buf = (random() != 1234) ? 1.0f : 0.567891234f;
   printf("result is %.9g while buf contains %.9g\n", buf * 0.00141589157f + (1.0f-buf) * 1.00000012f, buf);
+
   // Repeat when compiler knows that buf[0] is 1
   buf = 1.0f;
   printf("result is %.9g while buf contains %.9g\n", buf * 0.00141589157f + (1.0f-buf) * 1.00000012f, buf);
-  printf("random() returned %ld\n", r);
   return 0;
 }
