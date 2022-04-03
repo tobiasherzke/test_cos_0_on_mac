86e81b2601bcf0caf779719d903467a5742fe6d0
tobias <tobiasherzke@openmha.com>
2022-04-03 10:39:27 +0200

    Function def visible at point of call

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include "hanning.h"
 
+float MHAWindow::hanning(float x)  
+{
+    if( (x < -1) || (x >= 1) )
+        return 0;
+    return 0.5 + 0.5 * cos( M_PI*x );
+}
+
 int main(int, char**) {
   float lev_new = 0.00141589157f;
   float lev_old = 1.00000012f;
