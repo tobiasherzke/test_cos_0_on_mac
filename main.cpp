3a2f0189e5f40ebd73d22df7fe33022105d81e59
tobias <tobiasherzke@openmha.com>
2022-04-03 10:34:43 +0200

    Revert "No indirect function call"
    This reverts commit b2a0887a8b9dd18e47fe6bd3796bd5cda0fde059.

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main(int, char**) {
   float * buf = new float[1];
   float * results = new float[fragsize];
   std::fill_n(results, fragsize, 0.0f);
-  buf[0] = 1;//fun(x);
+  buf[0] = fun(x);
   for (unsigned k = 0; k < fragsize; ++k)
     results[k] += buf[0] * lev_new + (1.0f-buf[0]) * lev_old;
   printf("%.9g\n", results[0]);
