f07eb0d3e50a1cefa6fa00a64aff47402756bdfd
tobias <tobiasherzke@openmha.com>
2022-04-03 11:36:00 +0200

    Eliminate array, print random() return value

--- main.cpp
+++ main.cpp
@@ -2,11 +2,13 @@
 #include <cstdlib>
 
 int main(int, char**) {
-  float * buf = new float[1];
-  buf[0] = (random() != 1234) ? 1.0f : 0.567891234f;
-  printf("result is %.9g while buf contains %.9g\n", buf[0] * 0.00141589157f + (1.0f-buf[0]) * 1.00000012f, buf[0]);
+  auto r = random();
+  // buf will always contain 1.0 but compiler does not know.
+  float buf = (r != 1234) ? 1.0f : 0.567891234f;
+  printf("result is %.9g while buf contains %.9g\n", buf * 0.00141589157f + (1.0f-buf) * 1.00000012f, buf);
   // Repeat when compiler knows that buf[0] is 1
-  buf[0] = 1.0f;
-  printf("result is %.9g while buf contains %.9g\n", buf[0] * 0.00141589157f + (1.0f-buf[0]) * 1.00000012f, buf[0]);
+  buf = 1.0f;
+  printf("result is %.9g while buf contains %.9g\n", buf * 0.00141589157f + (1.0f-buf) * 1.00000012f, buf);
+  printf("random() returned %ld\n", r);
   return 0;
 }
