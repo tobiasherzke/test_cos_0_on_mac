36ec32147f59293fd884fa755eebd436506c28be
tobias <tobiasherzke@openmha.com>
2022-04-03 10:31:53 +0200

    Minimize includes

--- hanning.h
+++ hanning.h
@@ -1,23 +1,5 @@
 #include <algorithm>
 #include <cmath>
-#include <complex>
-#include <cstddef>
-#include <cstring>
-#include <dlfcn.h>
-#include <exception>
-#include <iostream>
-#include <limits>
-#include <list>
-#include <map>
-#include <netinet/in.h>
-#include <numeric>
-#include <sstream>
-#include <stdint.h>
-#include <stdlib.h>
-#include <string>
-#include <typeinfo>
-#include <type_traits>
-#include <unistd.h>
-#include <vector>
+#include <cstdio>
 
 namespace MHAWindow {float hanning(float);}
